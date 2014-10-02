3a75f33b7fd4d08e57553bd2e12e6e1e64870280
Stephan Hoogland <stephan@shoogland.com>
2014-10-02 14:51:01 +0200

    Initial Commit

---
+++ IOS-510537/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS-510537
+//
+//  Created by Stephan Hoogland on 02/10/14.
+//  Copyright (c) 2014 SHoogland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS-510537/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS-510537
+//
+//  Created by Stephan Hoogland on 02/10/14.
+//  Copyright (c) 2014 SHoogland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
