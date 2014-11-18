b0d5a2909803ad605fd323ef6d35dfa5066996ba
Stephan Hoogland <stephan@shoogland.com>
2014-11-18 13:37:49 +0100

    updated repo

--- Pods/Target Support Files/Pods-IOS-510537-SVPullToRefresh/Pods-IOS-510537-SVPullToRefresh-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IOS_510537_SVPullToRefresh : NSObject
-@end
-@implementation PodsDummy_Pods_IOS_510537_SVPullToRefresh
-@end
--- Pods/Target Support Files/Pods-IOS-510537/Pods-IOS-510537-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IOS_510537 : NSObject
-@end
-@implementation PodsDummy_Pods_IOS_510537
-@end
